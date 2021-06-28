7053d92b8d0fc36535e8ade0d18fe1b5aa7ddb42
NathanRodriguez04 <naterod26@gmail.com>
2021-06-28 15:28:30 -0500

    java

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
  */
 public class ConsoleExercises {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter("\n");
+//        Scanner scanner = new Scanner(System.in);
+//        scanner.useDelimiter("\n");
+//
+//        System.out.println("Enter your length, width, height: ");
+//
+//        float length = Float.parseFloat(scanner.next());
+///*
+//        float width = Float.parseFloat(scanner.next());
+//        float height = Float.parseFloat(scanner.next());
+//*/
+//
+//        double width = Double.parseDouble(scanner.next());
+//        double height = Double.parseDouble(scanner.next());
+//
+//        System.out.printf("For give length: %f, width: %f, height: %f \n", height, width, height);
+//        System.out.println("Area of given rectangle: " + (length * width));
+//        System.out.println("Perimeter of given rectangle: " + (2 * length * width));
+//        System.out.println("Volume of the room: "+ (width * height * length));
 
-        System.out.println("Enter your length, width, height: ");
-
-        float length = Float.parseFloat(scanner.next());
-/*
-        float width = Float.parseFloat(scanner.next());
-        float height = Float.parseFloat(scanner.next());
-*/
-
-        double width = Double.parseDouble(scanner.next());
-        double height = Double.parseDouble(scanner.next());
-
-        System.out.printf("For give length: %f, width: %f, height: %f \n", height, width, height);
-        System.out.println("Area of given rectangle: " + (length * width));
-        System.out.println("Perimeter of given rectangle: " + (2 * length * width));
-        System.out.println("Volume of the room: "+ (width * height * length));
+//        System.out.println("Enter room dimensions to calculate area and perimeter: ");
+//
+//        double length = Double.parseDouble(sc.next());
+//        double width = Double.parseDouble(sc.next());
+//        double height = Double.parseDouble((sc.next()));
+//        double area = length * width;
+//        double perimeter = (length * 2) + (width * 2);
+//        double volume = length * width * height;
+//
+//        System.out.printf("The area of your input is %.3f, while the perimeter is %.3f.\nAlso, the volume is %.3f", area, perimeter, volume);
     }
 }
