1fc69d35dab5106f1b315cd54b50ceea483cb120
NathanRodriguez04 <naterod26@gmail.com>
2021-06-29 14:12:11 -0500

    java

--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -2,38 +2,64 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringsLecture {
-    public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-//        System.out.println("Print your name");
-//        String input = scnr.next();
-//        String randomName = "Grady";
-//        if(input.equalsIgnoreCase(randomName)){
-//            System.out.println("Hey that's my name!");
+//    public static void main(String[] args) {
+//        Scanner scnr = new Scanner(System.in);
+////        System.out.println("Print your name");
+////        String input = scnr.next();
+////        String randomName = "Grady";
+////        if(input.equalsIgnoreCase(randomName)){
+////            System.out.println("Hey that's my name!");
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        String[] studentsArr =
+//                {"Amado", "Christian", "Christopher", "Corey", "Demetrio",
+//                        "Diamond", "Erik", "Grady", "Jesse", "Joshua",
+//                        "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
+//                        "Raul", "Ricardo", "Richard", "Robert", "Sam",
+//                        "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
+//                        "Casey", "Dorian", "Tristan", "Jordy"};
+//
+//        System.out.println("Enter your name");
+//        int randomNum = (int) (Math.random() * 28);
+//        String randomName = studentsArr[randomNum];
+//        String input = sc.next();
+//        while (!input.equalsIgnoreCase(randomName)) {
+//
+//            randomNum = (int) (Math.random() * 28);
+//            randomName = studentsArr[randomNum];
+//            System.out.printf("%s \n", randomName);
+//
+//        }
+//
+//
+//    }
+    //    TODO: you are a sports manager. Prompt your client who is training
+//     for a marathon how many miles they have to run today. You're the
+//     coach, so you decide how many miles they ran that day. create a method that takes in how many miles that have left in training and simulate that they check in with you after every 2 miles. print out
+//     how many miles they have left in training until they have 0 miles
+//     left.
 
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        String[] studentsArr =
-                {"Amado", "Christian", "Christopher", "Corey", "Demetrio",
-                        "Diamond", "Erik", "Grady", "Jesse", "Joshua",
-                        "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
-                        "Raul", "Ricardo", "Richard", "Robert", "Sam",
-                        "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
-                        "Casey", "Dorian", "Tristan", "Jordy"};
+    System.out.println("how many miles have you ran today?");
 
-        System.out.println("Enter your name");
-        int randomNum = (int) (Math.random() * 28);
-        String randomName = studentsArr[randomNum];
-        String input = sc.next();
-        while (!input.equalsIgnoreCase(randomName)){
+    int userMiles = sc.nextInt();
 
-            randomNum = (int) (Math.random() * 28);
-            randomName = studentsArr[randomNum];
-            System.out.printf("%s \n", randomName);
+    checkMiles(userMiles);
+    System.out.println("no more miles left");
+    }
 
-        }
+    public static void checkMiles(int miles) {
+if(miles > 0){
+    System.out.printf("you still have %s miles left.\n",miles);
+    miles -= 2;
 
-
-
-
+    System.out.printf("Miles: %s\n",miles);
+    checkMiles(miles);
+}
     }
 }
+
+
